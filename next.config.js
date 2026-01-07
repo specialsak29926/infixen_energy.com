396f91b1c7e99c3d5f627dd96cab17e1117017b4
sakshisonawane29 <sakshi@hashteelab.com>
2026-01-07 18:44:34 +0530

    Fix asset paths for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,9 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  basePath: '/infixen_energy.com',
+  assetPrefix: '/infixen_energy.com',
 }
 
 module.exports = nextConfig
