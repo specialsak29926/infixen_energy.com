a443cf32a4f6d3a83c010a16da0f925f84c4b50f
sakshisonawane29 <sakshi@hashteelab.com>
2026-01-07 18:15:30 +0530

    Configure static export and GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/infixen_energy.com',
+  assetPrefix: '/infixen_energy.com/',
 }
 
 module.exports = nextConfig
